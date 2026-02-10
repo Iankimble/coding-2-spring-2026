080d6fe66e775d148b40f351ec26319906c60880
Ian <38480036+Iankimble@users.noreply.github.com>
2026-02-10 12:37:59 +0000

    review

--- Unit 1/SpringReview.py
+++ Unit 1/SpringReview.py
@@ -1,7 +1,14 @@
-# Data Types
+# 1. Create a function that will check a users age and GPA and determine 
+# if they are qaulify to attend the job fair. Your function should 
+# take the user's age and GPA in as parameters. If the user
+# is above 17 and has atleast a 90 or above GPA they can go to the
+# job fair. Otherwise they are not permitted to go.
 
-# Operators
-
-# Functions
-
-# Lists
+# 2. Create a function that checks the price of an item, and depending 
+# on the item they will get a specific discount amount. Your function
+# should take in the item price as an input from the user. If the user's
+# item costs between 50 and 75 dollars, they will get a 15% discount.
+# If the item is above 75 dollars, they will recieve a 25% discount.
+# Your program should print out and inform the user how much of a 
+# discount they will get and what the new total for the item be with 
+# the applied discount. 
