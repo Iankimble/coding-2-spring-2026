fac27eba97a02d97e717572a643d10eff53720ac
Ian <38480036+Iankimble@users.noreply.github.com>
2026-05-05 13:29:38 +0000

    dictionaries lesson

---
+++ Unit 4/dictionaries.py
@@ -0,0 +1,48 @@
+# A dictionary is a collection type that uses key / value pairs to 
+# describe data. Its a simpler and faster form of objects. 
+
+
+# A dictionary is very similar to a class.
+# dictionaries ONLY focus on key value pairs. 
+# NO methods
+
+finals= {
+    "name": "Finals Testing",
+    "time": "9 AM",
+    "date":"May 11",
+    "grades":"9-11"
+}
+
+srProm= {
+    "name": "Senior Prom",
+    "time": "9 PM",
+    "date":"May 8",
+    "grades":"12"
+}
+
+srTrip= {
+    "name": "Senior Feild Trip",
+    "time": "10 AM",
+    "date":"May 22",
+    "grades":"12"
+}
+
+graduation= {
+    "name": "Graduation Day",
+    "time": "11AM",
+    "date":"May 27",
+    "grades":"12"
+}
+
+# dictionaries dont have indexes but rather they have keys
+# key are the name/property type that contains the actual data. 
+
+#print(graduation["date"])
+
+maySchedule = [finals, srProm, srTrip, graduation]
+
+print(maySchedule)
+
+for event in maySchedule:
+    print(event['name'])
+    
